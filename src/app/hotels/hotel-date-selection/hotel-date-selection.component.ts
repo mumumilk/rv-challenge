935ec534980fc99185475ab6ebc992a1bdd40020
samuel koch <samuel.souza.koch@gmail.com>
2018-05-03 22:45:04 -0300

    comecei layout da seleção de datas

---
+++ src/app/hotels/hotel-date-selection/hotel-date-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-hotel-date-selection',
+    templateUrl: './hotel-date-selection.component.html',
+    styleUrls: ['./hotel-date-selection.component.css']
+})
+export class HotelDateSelectionComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
--- src/app/hotels/hotels.module.ts
+++ src/app/hotels/hotels.module.ts
@@ -5,11 +5,24 @@ import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { HotelCardComponent } from './hotel-card/hotel-card.component';
 import { HotelStarsFilterComponent } from './hotel-stars-filter/hotel-stars-filter.component';
 import { HotelValueFilterComponent } from './hotel-value-filter/hotel-value-filter.component';
+import { HotelDateSelectionComponent } from './hotel-date-selection/hotel-date-selection.component';
 
 @NgModule({
-    declarations: [ HotelListComponent, HotelCardComponent, HotelStarsFilterComponent, HotelValueFilterComponent ],
+    declarations: [
+        HotelListComponent,
+        HotelCardComponent,
+        HotelStarsFilterComponent,
+        HotelValueFilterComponent,
+        HotelDateSelectionComponent
+    ],
     imports: [ CommonModule ],
-    exports: [ HotelListComponent, HotelCardComponent, HotelStarsFilterComponent, HotelValueFilterComponent ],
+    exports: [
+        HotelListComponent,
+        HotelCardComponent,
+        HotelStarsFilterComponent,
+        HotelValueFilterComponent,
+        HotelDateSelectionComponent
+    ],
     providers: [ HotelsService ],
 })
 export class HotelsModule {}
