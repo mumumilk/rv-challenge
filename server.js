431035f3409588598678c975c57130a16a4a7089
samuel koch <samuel.souza.koch@gmail.com>
2018-05-11 18:03:58 -0300

    adicionei compressão de arquivos no server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // server.js
 const express = require('express');
 const app = express();
+
+const compression = require('compression');
+app.use(compression());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
